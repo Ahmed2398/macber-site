b7b45056073e8357d4bd4fd14c85ae411c81bcd0
Ahmed Ashraf <57802370+Ahmed2398@users.noreply.github.com>
2022-08-30 13:26:56 +0200

    first commit

---
+++ src/js/master.js
@@ -0,0 +1,148 @@
+// FAV ICON
+const faviconEl = document.querySelector('link[rel="icon"]')
+
+// watch for changes 🕵️
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+mediaQuery.addEventListener('change', themeChange)
+
+// listener 👂
+function themeChange(event) {
+  if (event.matches) {
+    faviconEl.setAttribute('href', 'favicon-dark.png')
+  } else {
+    faviconEl.setAttribute('href', 'favicon-light.png')
+  }
+}
+
+
+// Loadind
+var Load = document.getElementById("Loading");
+var logoLoad = document.getElementById("logo-load");
+function loadfun() {
+  Load.style.display = "none";
+}
+// Create Menue
+// let openNav = function openNav() {
+
+//   document.getElementById("mySidenav").style.width = "200px";
+// }
+
+
+// let closeNav = function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+//   // document.body.style.width = "0";
+//     document.getSelection("mySidenav").style.width = "0";
+// }
+// END
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.body.style.backgroundColor = "white";
+  }
+// Show Slides
+
+
+
+// Button Scrolling
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
+
+//   console.log(e.target.getBoundingClientRect());
+
+//   console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+//   console.log(
+//     'height/width viewport',
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+// Nav navigation
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(height);
+
+// Step 3
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+// Step 2
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+// Step 4
+headerObserver.observe(header);
+// Home Header sticky
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Home Header  Nav-link Target
+document.querySelector(".nav-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // MAtching strategy
+  if (e.target.classList.contains(".nav-link")) {
+    const id = e.target.getAttribute("href");
+      document.querySelectorAll(id).scrollIntoView({ behavior: "smooth"});
+      
+  }
+});
+
+// Scroll up BTN
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// END
+
